fef1c5913bdc2143d7974e1695c5663a0c3c47f2
natashafarias2 <118220162+natashafarias2@users.noreply.github.com>
2025-11-03 00:36:01 -0300

    Add methods to print days of the week

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        printWeekDay(0);
+        printWeekDay(1);
+        printWeekDay(2);
+        printWeekDay(3);
+        printWeekDay(4);
+        printWeekDay(5);
+        printWeekDay(6);
+        printWeekDay(7);
+    }
+
+    public static void printDayOfWeek(int day) {
+
+        String dayOfWeek = switch (day) {
+            case 0 -> { yield "Sunday"; }
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid Day";
+        };
+
+        System.out.println(day + " stands for " + dayOfWeek);
+    }
+
+    public static void printWeekDay(int day) {
+
+        String dayOfWeek = "Invalid Day";
+        if (day == 0) {
+            dayOfWeek = "Sunday";
+        } else if (day == 1) {
+            dayOfWeek = "Monday";
+        } else if (day == 2) {
+            dayOfWeek = "Tuesday";
+        } else if (day == 3) {
+            dayOfWeek = "Wednesday";
+        } else if (day == 4) {
+            dayOfWeek = "Thursday";
+        } else if (day == 5) {
+            dayOfWeek = "Friday";
+        } else if (day == 6) {
+            dayOfWeek = "Saturday";
         }
+
+        System.out.println(day + " stands for " + dayOfWeek);
     }
 }
